a910e56c4f03353a758ae4ec10056ff3bfd431b3
[razamuzammil426@gmail.com] <razamuzammil426@gmail.com>
2022-10-19 18:01:41 +0500

    adding firbase

--- olx-clone/src/Components/PorductCart.jsx
+++ olx-clone/src/Components/PorductCart.jsx
@@ -1,9 +1,72 @@
 import React from "react";
 
 const PorductCart = () => {
-  return <div className="h-72 w-[24%] border-2 rounded-xl border-black">
-    
-  </div>;
+
+  const iconStyle =
+    "h-[40px] w-[40px] rounded-full bg-white flex items-center justify-center m-3 hover:bg-[#894af3] hover:text-white hover:scale-[1.1] ease-in duration-100 cursor-pointer";
+console.log(db)
+
+  return (
+    <div className="h-72 w-[24%] border-2 rounded-xl border-black">
+      <div className="flex-col items-center  justify-between">
+        <img src="../helpers/img/cu3.png" alt="product" className="h-[180px]" />
+        <div className="text-center">
+          <h5>{"Chiken"}</h5>
+          <p>${"15"}</p>
+        </div>
+      </div>
+      <div className={`flex items-center justify-center ease-in duration-100 `}>
+        <div className={iconStyle}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+            />
+          </svg>
+        </div>
+        <div className={iconStyle}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+            />
+          </svg>
+        </div>
+        <div className={iconStyle}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PorductCart;
