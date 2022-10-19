2bb695e6df7101b014397ec81808df27c21b04a2
[razamuzammil426@gmail.com] <razamuzammil426@gmail.com>
2022-10-19 16:52:13 +0500

    Footer done

--- olx-clone/src/Components/Footer.jsx
+++ olx-clone/src/Components/Footer.jsx
@@ -7,20 +7,20 @@ import AppGallery from "../images/iconAppGallery.svg";
 const Footer = () => {
   return (
     <div className="flex px-2">
-      <div className="bg-slate-300 flex flex-2 h-52">
+      <div className="bg-slate-300 flex flex-1.5 h-52">
         <img src={footerAd} alt="" className="object-cover" />
       </div>
-      <div className="bg-slate-300 flex-1 px-8  h-52 flex items-center ">
+      <div className="hidden bg-slate-300 flex-1 px-8  h-52 lg:flex items-center ">
         <div className="h-[80%] border-r-2">
-          <h1 className="font-bold pt-8 pb-3 text-3xl">TRY THE OLX APP</h1>
+          <h1 className="font-bold pt-2 pb-3 text-3xl">TRY THE OLX APP</h1>
           <p className="font-medium text-xl">
             Buy, sell and find just about anything using the app on your mobile.
           </p>
         </div>
       </div>
-      <div className="bg-slate-300 flex flex-1 h-52 ml-8">
-        <p>GET YOUR APP TODAY</p>
-        <div>
+      <div className="bg-slate-300 flex flex-col flex-1 h-52 ml-8 py-10">
+        <p className="font-bold">GET YOUR APP TODAY</p>
+        <div className="flex gap-3 justify-between py-12">
           <img
             src="../images/iconGooglePlay.svg"
             alt="App-Store"
